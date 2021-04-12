7dce1dcbf8bfe0f7953bd0088006c5bdb14fd2b6
ChristopherErb01 <christophererb01@gmail.com>
2021-04-12 14:04:03 -0400

    Just Added Noteservice stuff

--- DrywallCalc.Services/EmployeeService.cs
+++ DrywallCalc.Services/EmployeeService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DrywallCalc.Data;
+using DrywallCalc.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace DrywallCalc.Services
 {
-    class EmployeeService
+    public class EmployeeService
     {
 
         private readonly Guid _userId;
@@ -15,6 +17,53 @@ namespace DrywallCalc.Services
         {
             _userId = userId;
         }
+
+        public bool CreateEmployee(EmployeeCreate model)
+        {
+            var entity =
+                new Employee()
+                {
+                    Employee_Id = _userId,
+                    FullName = model.FullName,
+                    Title = model.Title,
+                    PayRate = model.PayRate,
+                    HireDate = DateTimeOffset.Now
+
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Employees.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+
+        }
+
+        //See all employees created by X user
+         public IEnumerable<EmployeeListItem> GetEmployees()
+         {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                    .Employees
+                    .Where(e => e.Employee_Id == _userId)
+                    .Select(
+                        e => new EmployeeListItem
+                        {
+                            FullName = e.FullName,
+                            Title = e.Title,
+                            PayRate = e.PayRate,
+                            HireDate = e.HireDate
+                        }
+
+                        );
+
+                return query.ToArray();
+            }    
+
+         }
+
 
 
 
